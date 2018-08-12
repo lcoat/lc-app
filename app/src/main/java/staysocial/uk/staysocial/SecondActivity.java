d9e2448056968e66ba8e224c2f9ce9dcc59eeaf2
Timothy_Long <tim_long123@hotmail.com>
2018-08-12 10:10:45 +0100

    initial commit

---
+++ app/src/main/java/staysocial/uk/staysocial/SecondActivity.java
@@ -0,0 +1,33 @@
+package staysocial.uk.staysocial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent myIntent = new Intent( SecondActivity.this, MainActivity.class);
+                myIntent.putExtra("key", "hi all, this has passed through correctly");
+                SecondActivity.this.startActivity(myIntent);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
